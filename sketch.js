674e383cffc67ec7e8d575cdb8f1b3a6a4ac3f3a
Srivatsan467 <63360214+Srivatsan467@users.noreply.github.com>
2020-05-19 21:23:05 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var  database;
+var playerCount,form,player,game;
+var gameState = 0;
+
+function setup(){
+  database = firebase.database();
+  createCanvas(500,500);
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+function draw(){
+  
+}
+
